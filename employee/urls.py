4e17e1470ce222b56d096550c5983f9b838752bf
Riya20khandelwal <riyakhandelwal204@gmail.com>
2024-08-22 16:26:37 +0530

    employee registration

--- employee/urls.py
+++ employee/urls.py
@@ -3,5 +3,7 @@ from .import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('registration', views.registration, name="registration"),
+    path("emp_login", views.emp_login, name="emp_login"),
     
 ]
--- employee/views.py
+++ employee/views.py
@@ -1,6 +1,30 @@
 from django.shortcuts import render, redirect
-
+from .models import *
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
+
+
+def registration(request):
+    error = ""
+    if request.method == "POST":
+        fn = request.POST['firstname']
+        ln = request.POST['lastname']
+        ec = request.POST['empcode']
+        em = request.POST['email']
+        pwd = request.POST['pwd']
+
+        try:
+            user = User.objects.create_user(first_name=fn, last_name=ln, username=em, password=pwd)
+            EmployeeDetail.objects.create(user=user, empcode=ec)
+            error = "no"
+
+        except:
+            error = "yes"
+
+    return render(request, 'registration.html', locals())
+
+
+def emp_login(request):
+    return render(request, 'emp_login.html')
